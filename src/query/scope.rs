3f2218a5f5fdb67a6a4fb5396a40a034a55e6e33
R. Miles McCain <0@rmrm.io>
2018-12-16 16:44:15 -0500

    Create scope

---
+++ src/query/scope.rs
@@ -0,0 +1,31 @@
+use common::pattern::{CompiledPattern, Pattern};
+use common::compilation::CompilableTo;
+use common::validation::Issue;
+
+pub struct Scope {
+    documents: Pattern,
+    content: ScopeContent
+}
+
+pub struct CompiledScope {
+    documents: CompiledPattern,
+    content: ScopeContent
+}
+
+#[derive(Copy, Clone)]
+pub enum ScopeContent {
+    Raw,
+    Text
+}
+
+impl CompilableTo<CompiledScope> for Scope {
+    fn compile(&self) -> Result<CompiledScope, Issue> {
+        match self.documents.compile() {
+            Ok(compiled_pattern) => Ok(CompiledScope {
+                documents: compiled_pattern,
+                content: self.content,
+            }),
+            Err(issue) => Err(issue)
+        }
+    }
+}
