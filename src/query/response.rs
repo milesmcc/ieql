d62829a16d82bded03cd0e1f25cc88ff21720780
R. Miles McCain <0@rmrm.io>
2018-12-16 16:44:03 -0500

    Create response definition

---
+++ src/query/response.rs
@@ -0,0 +1,39 @@
+use common::validation::{Issue, Validatable};
+
+pub struct Response {
+    kind: ResponseKind,
+    include: Vec<ResponseItem>,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum ResponseKind {
+    Full,
+    Partial,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum ResponseItem {
+    Excerpt,
+    Url,
+    Domain,
+    Mime,
+}
+
+impl Validatable for Response {
+    fn validate(&self) -> Option<Vec<Issue>> {
+        let mut issues: Vec<Issue> = Vec::new();
+        if self.kind == ResponseKind::Partial {
+            let disallowed_items = vec![ResponseItem::Excerpt, ResponseItem::Url];
+            for item in &self.include {
+                if disallowed_items.contains(&item) {
+                    issues.push(Issue::Error(format!("include `{:?}` is not allowed in partial responses", item)))
+                }
+            }
+        }
+        if issues.len() == 0 {
+            return None;
+        } else {
+            return Some(issues);
+        }
+    }
+}
