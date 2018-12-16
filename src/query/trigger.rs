be5ef14e687cee315dc04b0e3c7d0a39e6fb55da
R. Miles McCain <0@rmrm.io>
2018-12-16 16:44:37 -0500

    Create + implement triggers

---
+++ src/query/trigger.rs
@@ -0,0 +1,35 @@
+use common::pattern::{Pattern, CompiledPattern, PatternMatch};
+use common::compilation::CompilableTo;
+use common::validation::Issue;
+
+pub struct Trigger {
+    pattern: Pattern,
+    id: String,
+}
+
+pub struct CompiledTrigger {
+    pattern: CompiledPattern,
+    id: String,
+}
+
+impl CompilableTo<CompiledTrigger> for Trigger {
+    fn compile(&self) -> Result<CompiledTrigger, Issue> {
+        match self.pattern.compile() {
+            Ok(compiled_pattern) => Ok(CompiledTrigger {
+                pattern: compiled_pattern,
+                id: self.id.clone()
+            }),
+            Err(issue) => Err(issue)
+        }
+    }
+}
+
+impl CompiledTrigger {
+    pub fn quick_check(&self, other: &String) -> bool {
+        self.pattern.quick_check(other)
+    }
+
+    pub fn full_check(&self, other: &String) -> Option<PatternMatch> {
+        self.pattern.full_check(other)
+    }
+}
