e697727c721b4d649b5dee883aa1bcd77f484f1f
R. Miles McCain <0@rmrm.io>
2018-12-16 16:43:09 -0500

    Create + implement shared patterns

---
+++ src/common/pattern.rs
@@ -0,0 +1,64 @@
+use common::validation::{Validatable, Issue};
+use regex;
+use common::compilation::CompilableTo;
+
+pub struct Pattern {
+    content: String,
+    kind: PatternKind,
+}
+
+pub struct PatternMatch {
+    excerpt: String,
+    relevant: (usize, usize),
+}
+
+pub struct CompiledPattern {
+    regex: regex::Regex
+}
+
+pub enum PatternKind {
+    RegEx,
+    Raw
+}
+
+impl CompilableTo<CompiledPattern> for Pattern {
+    fn compile(&self) -> Result<CompiledPattern, Issue> {
+        let regex_pattern = match self.kind {
+            PatternKind::Raw => match regex::Regex::new(regex::escape(self.content.as_str()).as_str()) {
+                Ok(result) => result,
+                Err(_) => return Err(Issue::Error(String::from("escaped regex literal could not compile"))),
+            },
+            PatternKind::RegEx => match regex::Regex::new(&self.content.as_str()) {
+                Ok(result) => result,
+                Err(_) => return Err(Issue::Error(String::from("regex could not compile"))),
+            }
+        };
+        Ok(CompiledPattern {
+            regex: regex_pattern
+        })
+    }
+}
+
+impl CompiledPattern {
+    pub fn quick_check(&self, other: &String) -> bool {
+        self.regex.is_match(&other)
+    }
+
+    pub fn full_check(&self, other: &String) -> Option<PatternMatch> {
+        match self.regex.find(&other) {
+            Some(finding) => {
+                Some(PatternMatch {
+                    excerpt: other.clone(),
+                    relevant: (finding.start(), finding.end())
+                })
+            },
+            None => None
+        }
+    }
+}
+
+impl Validatable for Pattern {
+    fn validate(&self) -> Option<Vec<Issue>> {
+        unimplemented!();
+    }
+}
