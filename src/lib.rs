f31ab59c01d7e55304acb68ce8c675068945176a
R. Miles McCain <0@rmrm.io>
2018-12-20 17:46:05 -0500

    Create input mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
-
 extern crate regex;
 extern crate ron;
+extern crate url;
 
 pub mod common;
 pub mod query;
 pub mod output;
+pub mod input;
 pub mod scan;
 
 #[cfg(test)]
