5f8f625468d917cc624e007cac30367d5cc84c12
R. Miles McCain <0@rmrm.io>
2018-12-24 16:27:57 -0500

    Implement Display for Issue

--- src/common/validation.rs
+++ src/common/validation.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub trait Validatable {
     fn validate(&self) -> Option<Vec<Issue>>;
 }
@@ -6,4 +8,13 @@ pub trait Validatable {
 pub enum Issue {
     Warning(String),
     Error(String),
+}
+
+impl fmt::Display for Issue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Issue::Error(message) => write!(f, "(err): {}", message),
+            Issue::Warning(message) => write!(f, "(warning): {}", message),
+        }
+    }
 }
