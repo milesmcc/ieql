55fb20197a07bf06bd94c72f15a77d9de3444248
R. Miles McCain <0@rmrm.io>
2019-01-09 08:30:50 -0500

    Add public re-exports

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,11 @@ pub mod output;
 pub mod input;
 pub mod scan;
 
+pub use query::query::{CompiledQuery, Query};
+pub use output::output::Output;
+pub use scan::scanner::Scanner;
+pub use input::document::Document;
+
 #[cfg(test)]
 mod tests {
     #[test]
