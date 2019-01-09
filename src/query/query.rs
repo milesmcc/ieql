1f7edcab5002e267460fcad4b9d61d1d05b93eb5
R. Miles McCain <0@rmrm.io>
2019-01-09 09:03:03 -0500

    Add query documentation

--- src/query/query.rs
+++ src/query/query.rs
@@ -1,3 +1,5 @@
+//! This file contains functionality related to queries.
+
 use query::response::Response;
 use query::scope::{CompiledScope, Scope, ScopeContent};
 use query::threshold::{Threshold, ThresholdConsideration};
@@ -10,19 +12,58 @@ use regex::{Regex, RegexSet};
 
 use std::collections::HashMap;
 
+/// `Query` represents an uncompiled query. This type is
+/// typically interstitial; it cannot perform scans, and has
+/// little functionality apart from its ability to compile into
+/// a `CompiledQuery`.
+/// 
+/// This type is part of the public API, and therefore must
+/// comply with the structure defined in the specification.
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Query {
+    /// Represents the desired `Response` of the query when
+    /// it matches `Document`s. In other words, this is the
+    /// set of parameters for the `Output`.
+    /// 
+    /// For more information, see the `Response` documentation.
     pub response: Response,
+    /// Represents the documents and content that the query
+    /// is applicable to. The `Scope` allows users to specify
+    /// which URLs their query is applicable to, as well as
+    /// the type of content—`Text` or `Raw`—that should be passed
+    /// to the triggers.
+    /// 
+    /// For more information, see the `Scope` documentation.
     pub scope: Scope,
+    /// Represents the composition of trigger matches necessary
+    /// in order for a match to be made.
+    /// 
+    /// For more information, see the `Threshold` documentation.
     pub threshold: Threshold,
+    /// Represents the `Triggers` that will be checked against
+    /// the document and then processed by the `Threshold` in
+    /// order to determine whether a match is made.
+    /// 
+    /// For more information, see the `Trigger` documentation.
     pub triggers: Vec<Trigger>,
+    /// Represents the `id` of the query. This field is optional
+    /// but highly recommended, as it will be copied to the
+    /// outputs created by this query.
     pub id: Option<String>,
 }
 
+/// Represents a collection of queries. This type is useful in
+/// cases where many different queries are being compiled at
+/// once.
 pub struct QueryGroup {
     pub queries: Vec<Query>,
 }
 
+/// Represents a compiled query which is ready to scan (compiled)
+/// documents (`CompiledDocument`).
+/// 
+/// For more information about each of these fields, please see the
+/// `Query` documentation.
 #[derive(Clone)]
 pub struct CompiledQuery {
     pub response: Response,
@@ -32,16 +73,65 @@ pub struct CompiledQuery {
     pub id: Option<String>,
 }
 
+/// Represents a group of compiled queries. This type has several
+/// internal optimizations that makes scanning using a
+/// `CompiledQueryGroup` far more efficient than scanning using
+/// compiled queries alone.
+/// 
+/// Namely, this type provides:
+/// * proper multithreading support
+/// * runtime optimizations for query execution
+///
+/// For information about how each of these optimizations
+/// are implemented, see the documentation for
+/// `scan_concurrently()` and the various fields.
+/// 
+/// There are few cases—if not none at all—when a `CompiledQueryGroup`
+/// should be mutable.
 #[derive(Clone)]
 pub struct CompiledQueryGroup {
+    /// Contains the compiled queries that make up the compiled
+    /// query group.
+    /// 
+    /// Importantly, this vector is _highly ordered_,
+    /// meaning that changing the order of this array will lead
+    /// to the entire scanning system breaking.
     pub queries: Vec<CompiledQuery>,
+    /// Contains every single RegEx pattern of every query's
+    /// triggers.
+    /// 
+    /// This highly efficient RegEx matching system
+    /// allows for the `CompiledQueryGroup`'s scanning mechanism
+    /// to know in advance which of its queries will _potentially
+    /// match_ on a document, without having to execute every
+    /// individual query.
     pub regex_collected: RegexSet,
+    /// This index relates every RegEx pattern in `regex_collected`
+    /// to its source query in `queries`.
+    /// 
+    /// For example, the 1st element of this vector corresponds to the 
+    /// 1st RegEx pattern in `regex_collected` and denotes the index
+    /// of its source query in `queries`.
     pub regex_collected_query_index: Vec<usize>,
-    pub always_run_queries: Vec<CompiledQuery>, // for unoptimizable queries
-    pub regex_feed: ScopeContent
+    /// Contains the queries that cannot be optimized using the
+    /// methods above, and therefore must be run on every document.
+    /// 
+    /// These queries typically are those that contain some sort of
+    /// inverse boolean operator, therefore making it possible for
+    /// the query to match even when none of its triggers match.
+    /// 
+    /// Queries that have a threshold with a `requires` value of `0`
+    /// and queries whose `ScopeContent` doesn't match the majority
+    /// are also included here as unoptimizable.
+    pub always_run_queries: Vec<CompiledQuery>,
+    /// The type of content that should be fed to the RegEx patterns
+    /// in `regex_collected`.
+    pub regex_feed: ScopeContent,
 }
 
 impl CompilableTo<CompiledQuery> for Query {
+    /// Compiles the `Query` into a `CompiledQuery`. Like all compilation
+    /// operations, this is expensive.
     fn compile(&self) -> Result<CompiledQuery, Issue> {
         let scope = match self.scope.compile() {
             Ok(compiled) => compiled,
@@ -68,6 +158,8 @@ impl CompilableTo<CompiledQuery> for Query {
 }
 
 impl CompilableTo<CompiledQueryGroup> for QueryGroup {
+    /// Compiles the `QueryGroup` into a `CompiledQueryGroup`. Like
+    /// all compilation operations, this is expensive.
     fn compile(&self) -> Result<CompiledQueryGroup, Issue> {
         let OPTIMIZE_FOR_SCOPE_CONTENT = ScopeContent::Raw;
 
@@ -84,7 +176,8 @@ impl CompilableTo<CompiledQueryGroup> for QueryGroup {
             for consideration in &threshold.considers {
                 match consideration {
                     ThresholdConsideration::NestedThreshold(nested_threshold) => {
-                        let (nested_triggers, nested_always) = recursively_analyze_threshold(&nested_threshold);
+                        let (nested_triggers, nested_always) =
+                            recursively_analyze_threshold(&nested_threshold);
                         if nested_always {
                             is_always = true;
                         }
@@ -94,7 +187,7 @@ impl CompilableTo<CompiledQueryGroup> for QueryGroup {
                 }
             }
 
-            if threshold.inverse {
+            if threshold.inverse || (threshold.requires == 0) {
                 is_always = true;
             }
 
@@ -106,7 +199,8 @@ impl CompilableTo<CompiledQueryGroup> for QueryGroup {
                 Ok(compiled_query) => compiled_query,
                 Err(issue) => return Err(issue), // kill early; compilation is expensive!
             };
-            let (relevant_trigger_ids, is_inverse) = recursively_analyze_threshold(&query.threshold);
+            let (relevant_trigger_ids, is_inverse) =
+                recursively_analyze_threshold(&query.threshold);
             if is_inverse || (query.scope.content != OPTIMIZE_FOR_SCOPE_CONTENT) {
                 always_runs.push(compiled_query);
             } else {
@@ -124,7 +218,11 @@ impl CompilableTo<CompiledQueryGroup> for QueryGroup {
 
         let regex_set = match RegexSet::new(sub_regexes) {
             Ok(set) => set,
-            Err(_iss) => return Err(Issue::Error(String::from("unable to compile master regex set")))
+            Err(_iss) => {
+                return Err(Issue::Error(String::from(
+                    "unable to compile master regex set",
+                )))
+            }
         };
 
         Ok(CompiledQueryGroup {
@@ -132,14 +230,20 @@ impl CompilableTo<CompiledQueryGroup> for QueryGroup {
             regex_collected: regex_set,
             regex_collected_query_index: sub_regexes_index,
             always_run_queries: always_runs,
-            regex_feed: OPTIMIZE_FOR_SCOPE_CONTENT
+            regex_feed: OPTIMIZE_FOR_SCOPE_CONTENT,
         })
     }
 }
 
-impl From<CompiledQuery> for CompiledQueryGroup { // CompiledQueryGroup can represent a single query
+impl From<CompiledQuery> for CompiledQueryGroup {
+    /// This helper function creates a `CompiledQueryGroup`
+    /// for single queries, enabling multithreading support for
+    /// single queries without any significant 'hacks.'
     fn from(query: CompiledQuery) -> CompiledQueryGroup {
-        CompiledQueryGroup { // it will always be faster to just run the query than to perform optimizations designed with multiple queries in mind
+        CompiledQueryGroup {
+            // it will always be faster to just run the query
+            // than to perform optimizations designed with
+            // multiple queries in mind
             queries: vec![],
             regex_collected: RegexSet::new(vec![""]).unwrap(),
             regex_collected_query_index: vec![],
@@ -150,6 +254,12 @@ impl From<CompiledQuery> for CompiledQueryGroup { // CompiledQueryGroup can repr
 }
 
 impl Validatable for Query {
+    /// Validates the query, as well as all of its sub-components.
+    /// 
+    /// While this is an important mechanism for proper validation
+    /// (note that some invalid queries will still compile), one should
+    /// always double-check the results by actually compiling the query
+    /// and performing a test scan.
     fn validate(&self) -> Option<Vec<Issue>> {
         let mut issues: Vec<Issue> = Vec::new();
 
@@ -177,7 +287,7 @@ impl Validatable for Query {
                 if value == true {
                     issues.push(Issue::Warning(String::from("query will match if all triggers do not match; this can be dangerous in certain situations")));
                 }
-            },
+            }
             Err(issue) => issues.push(issue),
         };
         if issues.len() > 0 {
@@ -330,9 +440,7 @@ mod tests {
             ],
             id: Some(String::from("Test Trigger #2 (inverse)")),
         };
-        let group = QueryGroup {
-            queries: queries
-        };
+        let group = QueryGroup { queries: queries };
         group.compile();
     }
 }
