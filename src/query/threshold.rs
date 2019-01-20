53ee770da7574bd38e34a6ca99d37c4c825a2b9d
R. Miles McCain <0@rmrm.io>
2019-01-19 21:50:00 -0500

    Add proper threshold documentation

--- src/query/threshold.rs
+++ src/query/threshold.rs
@@ -1,20 +1,39 @@
+//! This document provides functionality related to
+//! thresholds.
+
 use std::collections::HashMap;
 use common::validation::Issue;
 
+/// The `Threshold` struct allows for the boolean output of
+/// triggers to be composed so that only certain combinations
+/// constitute a 'match.'
+/// 
+/// You can think of the `Threshold` as a boolean expression
+/// that defines when a query matches.
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub struct Threshold {
-    pub considers: Vec<ThresholdConsideration>, // todo: make this a reference
+    pub considers: Vec<ThresholdConsideration>,
     pub requires: usize,
     pub inverse: bool
 }
 
+/// A consideration by the threshold that evaluates to
+/// either `true` or `false`. This can be a `Trigger`
+/// identified by its `id`, or a `NestedThreshold`.
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub enum ThresholdConsideration {
+    /// Refers to a Trigger in the query by its ID.
     Trigger(String),
+    /// Contains a Threshold which will then itself
+    /// be independently evaluated by the scan engine.
     NestedThreshold(Threshold),
 }
 
 impl Threshold {
+    /// Evaluates the threshold based on the given data.
+    /// 
+    /// # Arguments
+    /// * triggers: a `HashMap` where the keys are Trigger IDs and the values are whether they matched or not
     pub fn evaluate(&self, triggers: &HashMap<&String, bool>) -> Result<bool, Issue> {
         let mut matched = 0;
         
