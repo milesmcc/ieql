6abd4185ba35a28731a696377aed5cb61e3283ac
R. Miles McCain <0@rmrm.io>
2019-01-19 21:49:01 -0500

    Expand imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! **The reference implementation for IEQL, an open 
+//! standard for monitoring Internet content**
+//! 
+//! This library is the reference implementation for IEQL
+//! (Internet Extensible Query Language, pronounced equal).
+//! IEQL is an open standard for monitoring and querying 
+//! Internet content designed to be fast, efficient, and scalable.
+
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
@@ -12,15 +20,12 @@ pub mod output;
 pub mod input;
 pub mod scan;
 
-pub use query::query::{CompiledQuery, Query};
+pub use common::pattern::{Pattern, PatternKind};
+pub use query::response::{Response, ResponseItem, ResponseKind};
+pub use query::scope::{Scope, ScopeContent};
+pub use query::threshold::{Threshold, ThresholdConsideration};
+pub use query::trigger::Trigger;
+pub use query::query::{Query, QueryGroup}
 pub use output::output::Output;
 pub use scan::scanner::Scanner;
 pub use input::document::Document;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
