0037a7c5da6d5941bfbedeee7ad50405f2305e80
R. Miles McCain <0@rmrm.io>
2019-01-19 21:49:53 -0500

    Add proper scope documentation

--- src/query/scope.rs
+++ src/query/scope.rs
@@ -1,10 +1,24 @@
+//! This file provides functionality related to scopes.
+
 use common::pattern::{CompiledPattern, Pattern};
 use common::compilation::CompilableTo;
 use common::validation::Issue;
 
+/// A `Scope` describes the kind of data that will be passed
+/// to the queries, and which queries will be invoked.
+/// 
+/// The name describes exactly what it suggests: the _scope_
+/// of a particular query.
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub struct Scope {
+    /// The scope's `Pattern` is applied to each URL being passed
+    /// through the scan engine. In order for a query to be run on
+    /// a particular piece of data, the pattern must match
+    /// that data's URL.
     pub pattern: Pattern,
+    /// The content defines the type of content that the query's
+    /// triggers will be run on. (Possible options include `Raw`
+    /// and `Text`; for more information, see `ScopeContent`.)
     pub content: ScopeContent
 }
 
@@ -14,9 +28,17 @@ pub struct CompiledScope {
     pub content: ScopeContent
 }
 
+/// Denotes a form of text data to be passed to the query.
 #[derive(Copy, Clone, Serialize, Deserialize, Debug, PartialEq)]
 pub enum ScopeContent {
+    /// The raw data—usually either HTML or utf8 extracted from
+    /// web data/PDFs.
     Raw,
+    /// Intelligently extracted text from the document. For HTML
+    /// documents, for example, the `Text` is found by passing the
+    /// content through an HTML engine and extracting _all_ the text.AsMut
+    /// 
+    /// Note that sometimes JavaScript text is also included.
     Text
 }
 
